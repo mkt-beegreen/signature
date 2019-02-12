c361763077471595b255e903f9aa6aa29a28a55d
do ha anh <marketing@beegreen.vn>
2019-02-12 16:10:55 +0700

    update style

---
+++ app.js
@@ -0,0 +1,37 @@
+$(document).ready(function($) {
+  var fullname       = $('#form-wrap').find('#fullname'),
+      position       = $('#form-wrap').find('#position'),
+      phone          = $('#form-wrap').find('#phone'),
+      email          = $('#form-wrap').find('#email-company'),
+      changeName     = $('#wrapper').find('#name'),
+      changePosition = $('#wrapper').find('#pos'),
+      changePhone    = $('#wrapper').find('#tel'),
+      changeEmail    = $('#wrapper').find('#email');
+  fullname.keyup(function() {
+    var value = $(this).val();
+    changeName.text(value);
+  }).keyup();
+  position.keyup(function() {
+    var value = $(this).val();
+    changePosition.text(value);
+  }).keyup();
+  phone.keyup(function() {
+    var value = $(this).val();
+    changePhone.html('<span>Hotline:</span><a href="tel:'+value+'" style="color: #02533e; text-decoration: none;"> '+value+'</a>');
+  }).keyup();
+  email.keyup(function() {
+    var value = $(this).val();
+    changeEmail.html('<span>Email:</span><a href="email:'+value+'" style="color: #02533e; text-decoration:  none;"> '+value+'</a>');
+  }).keyup();
+  var clipboard = new ClipboardJS('#btn-copy', {
+    target: function() {
+      return document.getElementById('wrapper');
+    }
+  });
+  clipboard.on('success', function(e) {
+    console.log(e);
+  });
+  clipboard.on('error', function(e) {
+    console.log(e);
+  });
+});
